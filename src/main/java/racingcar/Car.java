cd3da33a8ea335c34993aad6a8334f9f104a28ad
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-10-27 16:26:44 +0900

    feat(Car): 이동 상태 조회 기능 추가 및 메서드 가독성 개선
    - 게임 진행에 필요한 위치 정보 getter 구현
    - 변수명 컨벤션을 카멜케이스로 통일
    - 메서드 체이닝으로 이동 로직 개선

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,21 +5,25 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
 
     private final String name;
-    private int move_count = 0;
+    private int moveCount = 0;
 
     public Car(final String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getMoveCount() {
+        return this.moveCount;
+    }
+
     public void move() {
         int random = Randoms.pickNumberInRange(0, 9);
         if (random >= 4) {
-            this.move_count++;
+            this.moveCount++;
         }
-        System.out.print(name + " : ");
-        for (int i = 0; i < move_count; ++i) {
-            System.out.print("-");
-        }
-        System.out.print("\n");
+        System.out.println(name + " : " + "-".repeat(moveCount));
     }
 }
